7c5fff9ef0d49c9a5e2a43615e4faee132ebc6e1
Rancune <pitet.brian@gmail.com>
2020-02-01 16:36:12 +0100

    Adding Scripts
    Ajout des Scripts pour les Checkpoints

---
+++ Assets/Script/PlayerPos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+    private GameObject warningCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        warningCanvas = GameObject.FindGameObjectWithTag("Respawn");
+        transform.position = gm.lastCheckPointPos;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+       if(other.CompareTag("Enemy")){
+           warningCanvas.GetComponent<RetryWarning>().Pause();
+           Debug.Log("Dead ...");
+       }
+   }
+}
