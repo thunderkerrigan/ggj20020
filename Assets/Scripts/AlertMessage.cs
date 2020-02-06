020314c9e7f23d5cb6dff711629ff722b1426ea8
Rancune <pitet.brian@gmail.com>
2020-02-06 15:10:34 +0100

    Level Design Tweaked and Finished

---
+++ Assets/Scripts/AlertMessage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertMessage : MonoBehaviour
+{
+    public GameObject AlertUI;
+
+    public static bool GameIsPaused = false;
+    // Start is called before the first frame update
+
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+                Debug.Log("Alert Message Missin Platforms ...");
+                AlertUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameIsPaused = true;
+                
+        }
+    }
+
+    public void Resume()
+    {
+        AlertUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+}
